5ea9ff71f6707a8f1c116e23b913d89087fe4b37
Pavel <pal5555@mail.ru>
2023-09-08 15:35:55 +0300

    output to the data modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { detalModal } from "../../store/Person/personSlice";
  export const Modal = ()=>{
     const  modal = useSelector((state)=>state.persons.modal)
+    const  person = useSelector((state)=>state.persons.person)
     const dispatch = useDispatch()
     const closeModale = ()=>{
         dispatch(detalModal(false))
@@ -14,7 +15,14 @@ import { detalModal } from "../../store/Person/personSlice";
             <div className="modal">
             <div className="modal__window" >
                 <div className="modal__content">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro exercitationem iure eaque vitae eum soluta assumenda amet minima. Rerum, dicta.
+                 <div className="modal__images">
+                    <img src="https://vuzopedia.ru/storage/app/media/male.jpg" alt="avatar" className="modalImage" /></div>
+                </div>
+                <div className="modal__text">
+                   <div className="modal__item"><strong>Имя: </strong>{person.firstname}</div>
+                   <div className="modal__item"><strong>Фамилия: </strong>{person.lastname}</div>
+                   <div className="modal__item"><strong>Email: </strong>{person.email}</div>
+                   <div className="modal__item"><strong>Телефон: </strong>{person.phone}</div>
                 </div>
             </div>
             <div className="overlay" onClick={closeModale}></div>
