6af53ff7e07c653f77f1f862a24f462a77c52990
Pavel <pal5555@mail.ru>
2023-09-26 18:28:17 +0300

    sort data

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getPersons } from "../../store/Person/personSlice";
+import { getPersons, changePersons } from "../../store/Person/personSlice";
 import Fields from "../fields/fields";
 import { Search } from "../search/search";
 
@@ -13,18 +13,32 @@ const Table = ()=>{
 
     const dispatch = useDispatch()
     const persons = useSelector((state)=>state.persons.list)
+
     const [data, setData] = useState([])
 
     useEffect(()=>{
       
           dispatch(getPersons())
+        
           setData(persons)
-          //console.log(data)
+          console.log(persons)
          
         
         
      
     } ,[])
+      const sortData= (field)=>{
+        let arraySort = persons.concat()
+
+       let  array = arraySort.sort((a, b)=>{
+          return a.field>b.field? 1: -1
+        })
+        dispatch(changePersons(array))
+        console.log(array)
+
+        
+      }
+  
     return(
 
       
@@ -33,10 +47,22 @@ const Table = ()=>{
       <div className="table">
         <div className="table__wrapper">
           <div className="table__header ">
-            <div className="table__field">Имя <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" /></div>
-            <div className="table__field">Фамилия  <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" /></div>
-            <div className="table__field">Email  <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" /></div>
-            <div className="table__field">Телефон  <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" /></div>
+            <div className="table__field" onClick={()=>{sortData('firstname')}}>
+              Имя 
+              <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" />
+              </div>
+            <div className="table__field" onClick={()=>{sortData('lastname')}}>
+              Фамилия  
+              <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" />
+              </div>
+            <div className="table__field" onClick={()=>{sortData('email')}} >
+              Email  
+              <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" />
+              </div>
+            <div className="table__field" onClick={()=>{sortData('phone')}}>
+              Телефон  
+              <img src="https://icon-library.com/images/img_274238_155.png" alt="icon" className="table__icon" />
+              </div>
           </div>
         </div>
         <Fields />
